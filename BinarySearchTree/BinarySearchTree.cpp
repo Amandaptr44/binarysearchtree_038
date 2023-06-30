7fbbf9cdaa95a37978c05301c188c734f0cbf87a
Amandaptr44 <amandaptr86@gmail.com>
2023-06-30 22:37:44 +0700

    menyimpan karakter yang dimasukkan pengguna dan membaca input

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -125,6 +125,15 @@ int main()
 	{
 		cout << "\nMenu" << endl;
 		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perfom inorder traversal" << endl;
+		cout << "3. Perform preorder transversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
 
 	}
 }
